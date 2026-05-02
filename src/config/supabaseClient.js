35fb66cb27a9ea5f38a611a55df783ced9b6f48b
Job Parado <jobparado06@gmail.com>
2026-05-02 19:35:46 +0800

    fixes of key and url

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || "https://lbkfdzzctzgsmdnmyjxi.supabase.co";
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY || "sb_publishable_MsSoKbm33DAVez-zG7w2tQ_TQOx_FLa";
+const SUPABASE_URL ="https://lbkfdzzctzgsmdnmyjxi.supabase.co";
+const SUPABASE_ANON_KEY ="sb_publishable_MsSoKbm33DAVez-zG7w2tQ_TQOx_FLa";
 
 
 if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
