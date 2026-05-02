02ae7c6fa2ecf6641935bf2a75491174f2d7d9c8
Job Parado <jobparado06@gmail.com>
2026-05-02 19:13:35 +0800

    fixes to change the display of supabse url and anon key

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,9 +1,9 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-const SUPABASE_URL = "https://lbkfdzzctzgsmdnmyjxi.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_MsSoKbm33DAVez-zG7w2tQ_TQOx_FLa";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-// Verify keys are set
+
 if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
   console.error("Missing Supabase credentials in config");
 }
