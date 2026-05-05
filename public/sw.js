361e5c711732527126783be90d221a978caf6fe3
Job Parado <jobparado06@gmail.com>
2026-05-05 13:37:19 +0800

    refactor: update manifest link paths and add favicon images for PWA support

--- public/sw.js
+++ public/sw.js
@@ -4,17 +4,17 @@ const ASSETS = [
   '/index.html',
   '/create.html',
   '/homePage.html',
-  '/css/homepage.css',
-  '/css/LoginCreate.css',
+  // '/css/homepage.css',
+  // '/css/LoginCreate.css',
   '/images/favicon_io/favicon.ico',
   '/images/favicon_io/android-chrome-192x192.png',
-  '/images/favicon_io/android-chrome-512x512.png',
+  '/images/favicon_io/android-chrome-512x512.png'
 
-  '/src/homePage.js',
-  '/src/account/account.js',
-  '/src/account/loginEmail.js',
-  '/src/account/loginGoogle.js',
-  '/src/account/registerEmail.js'
+  // '/src/homePage.js',
+  // '/src/account/account.js',
+  // '/src/account/loginEmail.js',
+  // '/src/account/loginGoogle.js',
+  // '/src/account/registerEmail.js'
 ];
 
 self.addEventListener('install', (event) => {
